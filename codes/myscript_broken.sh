f812e6e4f1d515daf9762c876c2a2088de3b02fe
Ali Snedden <apsnedden@gmail.com>
2019-04-19 08:15:43 -0400

    added grey background for shell scripts

--- codes/myscript_broken.sh
+++ codes/myscript_broken.sh
@@ -2,7 +2,7 @@
 #SBATCH --cpus-per-task=2
 #SBATCH --partition=himem
 #SBATCH --mail-user=First.Last@gmail.com
-#SBATCH --mail-type=FAIL,REQUEUE,TIME_LIMIT_90
+#SBATCH --mail-type=FAIL,TIME_LIMIT_90
 set -e
 echo "I'm looking in the forbidden location"
 ls /root/
